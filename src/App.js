2e27dab6b462c9704f2afb55fa6917b2798920ef
Jay Peek <73311799+ChefJayPeek@users.noreply.github.com>
2021-01-14 13:23:46 -0600

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './css/App.css';
 
+// components
+
+import NavBar from "./components/navbar/NavBar";
+import Jumbotron from "./components/jumbotron/Jumbotron";
+import MainPage from "./containers/MainPage";
+
+
+// app
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={process.env.PUBLIC_URL + "/"}>
+      <NavBar />
+      <Jumbotron />
+      <Route exact path="/" component={MainPage} />
+    </Router>
   );
 }
 
+// export
 export default App;
---
+++ src/components/item/Item.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Item = (props) => {
+  // Generate e-mail addresses
+  function makeEmailAddress(props) {
+    var employeeName = props.employee_name.toLowerCase().split(" ");
+    var EmailAddress = `${employeeName[0]}.${employeeName[1]}@company.com`;
+    return EmailAddress;
+  }
+
+  // Generate phone numbers
+  function makePhoneNumber() {
+    var fragment1 = Math.floor(Math.random() * 800 + 200);
+    var fragment2 = Math.floor(Math.random() * 1000);
+    var fragment3 = Math.floor(Math.random() * 10000);
+    var PhoneNumber = `(${formatNumber(fragment1)}) ${formatNumber(fragment2)}-${formatNumber(fragment3, 4)}`;
+    return PhoneNumber;
+  }
+
+  // © Ben
+  // Phone number random 0 first fix
+  function formatNumber(num, len = 3) {
+    num = num.toString();
+    if (num.length < len) { // 1: length 1
+      var count = len - num.length; // count = 2
+      while (count) {
+        num = "0" + num; // 01, // 001
+        count--; // count = 1, count = 0
+      }
+    }
+    return num; // 001
+  }
+
+  return (
+    <div className="row item-row">
+      <div className="col-sm-1">{props.id}</div>
+      <div className="col-md-3">{props.employee_name}</div>
+      <div className="col-md-5">{makeEmailAddress(props)}</div>
+      <div className="col-md-2">{makePhoneNumber()}</div>
+      <div className="col-sm-1">{props.employee_age}</div>
+    </div>
+  );
+};
+
+export default Item;
